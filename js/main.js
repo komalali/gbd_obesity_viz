825fb95c5e200712e1c1a4d41ad723377c67f609
Komal Ali <komalali@uw.edu>
2017-11-06 23:45:33 -0800

    added the super region buttons and rank data.
    TODO:
    1) Make buttons work to highlight super region.
    2) Style the tooltip div better.
    3) Emphasize focus on numbers in the tooltip (bold?)

--- js/main.js
+++ js/main.js
@@ -21,13 +21,23 @@ var valueline = d3.line()
                   .curve(d3.curveNatural);
 
 // tooltip div
-var div = d3.select('.main').append('div')
+var div = d3.select('.svg-container').append('div')
                             .attr('class', 'tooltip container')
                             .attr('width', width + margin.left + margin.right)
-                            .style('opacity', 0);
+                            .style('opacity', 0.9);
+
+// add information about global obesity
+   div.append('h5')
+       .text('Global');
+
+   div.append('p')
+        .text('High BMI is associated with a huge variety of health problems, ranging from heart disease to diabetes to cancers.');
+
+   div.append('p')
+       .text('Between 1990 and 2013, the prevalence of obesity (defined as the percentage of the population with a BMI > 30) has risen from 9.3% to 12.0% globally, a relative increase of 29%.');
 
 // create the svg object inside the body
-var svg = d3.select('.main').append('svg')
+var svg = d3.select('.svg-container').append('svg')
                             .attr('width', width + margin.left + margin.right)
                             .attr('height', padding + height + margin.top + margin.bottom)
                            .append('g')
@@ -57,27 +67,36 @@ d3.csv('data/data.csv', function(error, data) {
 
         var location = d.key,
             mean_1990 = d.values[0].mean,
-            mean_2013 = d.values[5].mean;
+            mean_2013 = d.values[5].mean,
+            global_rank = +d.values[5].global_rank,
+            super_region_rank = +d.values[5].super_region_rank;
 
         svg.append('g')
             .attr('class', 'trendline')
+            .append('path')
+            .attr('class', 'line')
+            .attr('d', valueline(d.values))
             .on("mouseover", function() {
                 div.transition()
                     .duration(200)
                     .style('opacity', 0.9);
 
-                div.html(d.key)
+                div.html('')
+                    .append('h5')
+                    .text(function() { return d.key; })
                     .append('p')
                     .text('In 1990, ' + mean_1990 + '% of the population of ' + location + ' was obese. ' +
-                          'By 2013, this number had changed to ' + mean_2013 + '%, a relative change of ' +
-                          ((mean_2013 - mean_1990) * 100 /mean_1990).toFixed(1) + '%.');
+                        'By 2013, this number had changed to ' + mean_2013 + '%, a relative change of ' +
+                        ((mean_2013 - mean_1990) * 100 /mean_1990).toFixed(1) + '%.');
 
                 div.append('p')
-                    .text('In 2013, ' + location + ' had a prevalence of ' + (mean_2013 - 12).toFixed(1) + '% higher than the global average of 12%.')
-            })
-            .append('path')
-            .attr('class', 'line')
-            .attr('d', valueline(d.values));
+                    .text('In 2013, ' + location + ' had a prevalence of ' + (mean_2013 - 12).toFixed(1) +
+                          '% higher than the global average of 12%. ');
+
+                div.append('p')
+                    .text(location + ' ranks in at #' + global_rank +
+                          ' most obese country globally, and #' + super_region_rank + ' in the super region.');
+            });
 
     });
 
@@ -101,5 +120,22 @@ d3.csv('data/data.csv', function(error, data) {
         .attr('transform', 'translate(' + (width/2) + ',' + (height + padding * 3.5) + ')')
         .text('Year');
 
+    // nest the data by super region
+    var dataNestSuper = d3.nest()
+        .key(function(d) { return d.super_region_name; })
+        .entries(data);
+
+    // loop through each super_region
+
+    dataNestSuper.forEach(function(d) {
+        var super_region = d.key;
+
+        d3.select('.buttonHolder')
+            .append('button')
+            .attr('class', 'btn btn-primary btn-sm')
+            .text(function() { return super_region; })
+    });
+
 
 });
+
